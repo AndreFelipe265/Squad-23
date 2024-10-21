c3cde62cb0b466c3e2c34dba51e838a7667df339
Abraão <abraaor048@gmail.com>
2024-10-21 11:22:36 -0300

    Co-authored-by: Cristhian Guelrith <cristhianguelrith@users.noreply.github.com>

--- fundamentos/src/components/Main.jsx
+++ fundamentos/src/components/Main.jsx
@@ -1,32 +1,32 @@
-import React,{ useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 //css
 
-import './Main.css'
+import "./Main.css";
 
 const Main = () => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/token/check', {
-      method: 'GET',
+    fetch("http://localhost:5000/api/token/check", {
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
-      }
+        "Content-Type": "application/json",
+      },
     })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Erro na requisição: ' + response.statusText);
-      }
-      return response.json();
-    })
-    .then(data => {
-      setData(data);
-    })
-    .catch(error => {
-      setError(error);
-    });
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erro na requisição: " + response.statusText);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        setError(error);
+      });
   }, []);
 
   if (error) {
@@ -40,12 +40,12 @@ const Main = () => {
   return (
     <div>
       <main>
-            <h1>INFORMAÇÕES:</h1>
-            <p>NOME: {data.name}</p>
-            <p>EMAIL: {data.email}</p>
+        <h1>INFORMAÇÕES:</h1>
+        <p>NOME: {data.name}</p>
+        <p>EMAIL: {data.email}</p>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
