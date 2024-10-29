54aec40018fc9008eedc73c9ff31d937e1359303
Fuelwild796 <165859108+Fuelwild796@users.noreply.github.com>
2024-10-29 09:47:54 -0300

    arquivos que não são uteis

--- jetsales/public/background.js
+++
@@ -1,8 +0,0 @@
-
-// Usar o icone para abrir a ferramenta lateral
-const GOOGLE_ORIGIN = 'https://www.google.com';
-
-
-chrome.sidePanel
-  .setPanelBehavior({ openPanelOnActionClick: true })
-  .catch((error) => console.error(error));
