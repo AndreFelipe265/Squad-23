cb51195be4dc8cbb1e800b345fb50ef1fb6eebfd
Abraão <abraaor048@gmail.com>
2024-11-05 09:53:35 -0300

    Revert "Merge pull request #1 from AndreFelipe265/Abra4o-mexendo"
    This reverts commit 296bfeb638ddc089955e72db75697f66f9b521b3, reversing
    changes made to 83a91dbcee4010465cf7a568bd77b89172a4400d.

--- fundamentos/jet_sales_backend/server.js
+++ fundamentos/jet_sales_backend/server.js
@@ -1,53 +1,72 @@
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
-require("dotenv").config();
+
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-const url = 'https://crm.rdstation.com/api/v1/token/check?token=6709bdde82747300196dadec';
+const urlTOKEN = 'https://crm.rdstation.com/api/v1/token/check?token=6709bdde82747300196dadec';
+const urlCONTATOS = 'https://crm.rdstation.com/api/v1/contacts?token=6709bdde82747300196dadec'
+const urlEMPRESA = 'https://crm.rdstation.com/api/v1/organizations?token=6709bdde82747300196dadec'
 
 app.use(cors());
 
-// Função genérica para requisições
-async function fetchData(url, res) {
-	try {
-		const response = await axios.get(url, {
-			headers: {
-				Authorization: `Bearer ${process.env.RD_TOKEN}`,
-				"Content-Type": "application/json",
-			},
-		});
-		res.json(response.data);
-	} catch (error) {
-		console.error("Erro ao fazer a requisição:", error.message);
-		res.status(500).json({
-			error: "Erro ao buscar dados da API da RD Station",
-		});
-	}
-}
+//TOKEN
+app.get('/api/token/check', async (req, res) => {
+  try {
+    const response = await axios.get(urlTOKEN, {
+      headers: {
+        'Authorization': `Bearer ${process.env.RD_TOKEN}`,
+        'Content-Type': 'application/json',
+      }
+    });
 
-// Endpoints usando a função genérica
-app.get("/api/token/check", (req, res) => {
-	const url =
-		"https://crm.rdstation.com/api/v1/token/check?token=6709bdde82747300196dadec";
-	fetchData(url, res);
+ 
+    res.json(response.data);
+  } catch (error) {
+    console.error('Erro ao fazer a requisição:', error.message);
+    res.status(500).json({ error: 'Erro ao buscar dados da API da RD Station' });
+  }
 });
 
+//CONTATOS
+app.get('/api/contacts', async (req, res) => {
+  try {
+    const response = await axios.get(urlCONTATOS, {
+      headers: {
+        'Authorization': `Bearer ${process.env.RD_TOKEN}`,
+        'Content-Type': 'application/json',
+      }
+    });
 
-app.get("/api/contacts", (req, res) => {
-	const url =
-		"https://crm.rdstation.com/api/v1/contacts?token=6709bdde82747300196dadec";
-	fetchData(url, res);
+ 
+    res.json(response.data);
+  } catch (error) {
+    console.error('Erro ao fazer a requisição:', error.message);
+    res.status(500).json({ error: 'Erro ao buscar dados da API da RD Station' });
+  }
 });
 
-app.get("/api/organizations", (req, res) => {
-	const url =
-		"https://crm.rdstation.com/api/v1/organizations?token=6709bdde82747300196dadec";
-	fetchData(url, res);
+//EMPRESA
+
+app.get('/api/organizations', async (req, res) => {
+  try {
+    const response = await axios.get(urlEMPRESA, {
+      headers: {
+        'Authorization': `Bearer ${process.env.RD_TOKEN}`,
+        'Content-Type': 'application/json',
+      }
+    });
+
+ 
+    res.json(response.data);
+  } catch (error) {
+    console.error('Erro ao fazer a requisição:', error.message);
+    res.status(500).json({ error: 'Erro ao buscar dados da API da RD Station' });
+  }
 });
 
 app.listen(PORT, () => {
-	console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
