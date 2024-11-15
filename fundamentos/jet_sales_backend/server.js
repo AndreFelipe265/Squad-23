cee09487b1dae4fcb1e76ad9b69fab74e46d909b
Roque <165859108+Fuelwild796@users.noreply.github.com>
2024-11-15 08:22:54 -0300

    Revert "Merge branch 'main' of https://github.com/AndreFelipe265/Squad-23"
    This reverts commit dedcaae7d36230b8bda773b0e8bd035182c4fd6c, reversing
    changes made to 6e86099e4ef41742be7711c6637441c9482561bd.

--- fundamentos/jet_sales_backend/server.js
+++ fundamentos/jet_sales_backend/server.js
@@ -11,50 +11,16 @@ const urlCONTATOS = 'https://crm.rdstation.com/api/v1/contacts?token=6709bdde827
 const urlEMPRESA = 'https://crm.rdstation.com/api/v1/organizations?token=6709bdde82747300196dadec'
 const urlNEGOCIAÇÃO = 'https://crm.rdstation.com/api/v1/deals?token=6709bdde82747300196dadec'
 
-//Token Rd station
 app.use(cors());
-app.use(express.json());
-app.use((req, res, next) => {
-  const tokenRD = req.headers['authorization'];
-  if (tokenRD) {
-    req.tokenRD = tokenRD.replace('Bearer ', ''); 
-  }
-  next();
-});
-
-//TOKEN E TICKET JETSALES
-let token_jetsales = ''
-let ticket_jetsales = ''
-
-app.post('/api/enviarToken', async (req, res) => {
-
-  const { token } = req.body;
-  token_jetsales = token
-  
-  if (!token) {
-    return res.status(400).json({ status: 'error', message: 'Token não enviado!' });
-  }})
-
-app.post('/api/enviarTicket', async (req, res) => {
-
-    const { ticket } = req.body;
-    ticket_jetsales = ticket
-    console.log("Ticket recebido no back-end:", ticket_jetsales);
-    
-    if (!ticket) {
-      return res.status(400).json({ status: 'error', message: 'Ticket não enviado!' });
-    }})
-
-
-
 
 //JETSAES
+
 app.get('/users',async (req, res)=>{
   try {
     const response = await fetch("https://chatapi.jetsalesbrasil.com/users/?pageNumber=1&hasMore=true", {
       headers: {
         "accept": "application/json, text/plain, */*",
-        "authorization": `Bearer ${token_jetsales}` 
+        "authorization": `Bearer ${process.env.JETSALES_TOKEN}` 
       },
       method: "GET"
     });
@@ -63,36 +29,46 @@ app.get('/users',async (req, res)=>{
   } catch (error) {
     res.status(500).json({ error: 'Erro ao buscar dados da API da JETSALES' });
   }
+  
+  
 });
 
 
-app.get('/ticket', async (req, res) => {
+app.get('/ticket',async (req, res)=>{
   try {
-    const response = await fetch(`https://chatapi.jetsalesbrasil.com/tickets/${ticket_jetsales}?id=${ticket_jetsales}`, {
+    const response = await fetch(`https://chatapi.jetsalesbrasil.com/tickets/${process.env.JETSALES_ID}?id=${process.env.JETSALES_ID}`, {
       headers: {
         "accept": "application/json, text/plain, */*",
-        "authorization": `Bearer ${token_jetsales}` 
+        "authorization": `Bearer ${process.env.JETSALES_TOKEN}` 
       },
       method: "GET"
     });
-    const data = await response.json();
-    console.log("Dados do ticket recebidos da API:", data); // Exibe a resposta da API de Jetsales
+    const data = await response.json()
     res.json(data);
   } catch (error) {
-    console.error("Erro ao buscar dados da API da JETSALES:", error);
     res.status(500).json({ error: 'Erro ao buscar dados da API da JETSALES' });
   }
+  
+  
 });
+
+
+
+
+
+
 
 //TOKEN
 app.get('/api/token/check', async (req, res) => {
   try {
     const response = await axios.get(urlTOKEN, {
       headers: {
-        'Authorization': `Bearer ${req.tokenRD}`,
+        'Authorization': `Bearer ${process.env.RD_TOKEN}`,
         'Content-Type': 'application/json',
       }
     });
+
+ 
     res.json(response.data);
   } catch (error) {
     console.error('Erro ao fazer a requisição:', error.message);
@@ -105,7 +81,7 @@ app.get('/api/contacts', async (req, res) => {
   try {
     const response = await axios.get(urlCONTATOS, {
       headers: {
-        'Authorization': `Bearer ${req.tokenRD}`,
+        'Authorization': `Bearer ${process.env.RD_TOKEN}`,
         'Content-Type': 'application/json',
       }
     });
@@ -124,26 +100,7 @@ app.get('/api/organizations', async (req, res) => {
   try {
     const response = await axios.get(urlEMPRESA, {
       headers: {
-        'Authorization': `Bearer ${req.tokenRD}`,
-        'Content-Type': 'application/json',
-      }
-    });
-
- 
-    res.json(response.data);
-  } catch (error) {
-    console.error('Erro ao fazer a requisição:', error.message);
-    res.status(500).json({ error: 'Erro ao buscar dados da API da RD Station' });
-  }
-});
-
-//NEGOCIAÇÃO
-
-app.get('/api/deals', async (req, res) => {
-  try {
-    const response = await axios.get(urlNEGOCIAÇÃO, {
-      headers: {
-        'Authorization': `Bearer ${req.tokenRD}`,
+        'Authorization': `Bearer ${process.env.RD_TOKEN}`,
         'Content-Type': 'application/json',
       }
     });
@@ -158,4 +115,23 @@ app.get('/api/deals', async (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+//NEGOCIAÇÃO
+
+app.get('/api/deals', async (req, res) => {
+  try {
+    const response = await axios.get(urlTOKEN, {
+      headers: {
+        'Authorization': `Bearer ${process.env.RD_TOKEN}`,
+        'Content-Type': 'application/json',
+      }
+    });
+
+ 
+    res.json(response.data);
+  } catch (error) {
+    console.error('Erro ao fazer a requisição:', error.message);
+    res.status(500).json({ error: 'Erro ao buscar dados da API da RD Station' });
+  }
 });
